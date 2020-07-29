d3e4fb7f22f23bfd199ca2b422484b99f10aa9b4
WilliamJuang <juangw@umich.edu>
2020-07-29 09:04:51 -0400

    setup lint

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 8080
+import express = require("express");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const port = 8080;
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
